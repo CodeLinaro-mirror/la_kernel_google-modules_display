8561bd5d6a7645ea50ad4de3199d7ed18c0963d8
André Draszik <draszik@google.com>
2025-03-31 06:31:26 +0100

    drm: samsung: remove driver date from struct drm_driver (v6.14 prep)
    Upstream has stopped using the driver initialized date in upstream
    commit [1] and subsequently fully removed the ::date field from
    struct drm_driver.
    
    Follow this change here.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=7fb8af6798e8 [1]
          ("drm: deprecate driver date")
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=cb2e1c2136f7 [2]
          ("drm: remove driver date from struct drm_driver and all drivers")
    Test: TH
    Bug: 407436911
    Change-Id: I0ecd608dc1a3cae2b9e70862e0dcb4ca893ad98b
    Signed-off-by: André Draszik <draszik@google.com>
    Signed-off-by: Tudor Ambarus <tudordana@google.com>

--- samsung/exynos_drm_drv.c
+++ samsung/exynos_drm_drv.c
@@ -43,7 +43,6 @@
 
 #define DRIVER_NAME	"exynos"
 #define DRIVER_DESC	"Samsung SoC DRM"
-#define DRIVER_DATE	"20110530"
 #define DRIVER_MAJOR	1
 #define DRIVER_MINOR	0
 
@@ -877,7 +876,6 @@ static struct drm_driver exynos_drm_driver = {
 	.fops			   = &exynos_drm_driver_fops,
 	.name			   = DRIVER_NAME,
 	.desc			   = DRIVER_DESC,
-	.date			   = DRIVER_DATE,
 	.major			   = DRIVER_MAJOR,
 	.minor			   = DRIVER_MINOR,
 };
